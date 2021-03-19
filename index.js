5c47911995afe1aefe01a347199b633ea9351dae
remy29 <arthur.d.remy@gmail.com>
2021-03-19 15:48:19 -0700

    added basic auth

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const app = express();
 const db = require('./db/queries');
+const basicAuth = require('express-basic-auth')
 const getUnconfirmedTrans = require('./helpers/api-request');
+const app = express();
 const port = 3000;
+// YWRtaW46bmV0LWNlbnRz
+
+app.use(basicAuth({
+  users: { 'admin': 'net-cents' },
+  unauthorizedResponse: getUnauthorizedResponse
+}))
+
+function getUnauthorizedResponse(req) {
+  return req.auth
+      ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
+      : 'No credentials provided'
+}
 
 app.use(express.json());
 app.use(
@@ -20,7 +33,14 @@ app.get('/biggest', db.getCurrentBiggest);
 app.listen(port, () => {
 	console.log(`App running on port ${port}.`);
 
-	setInterval(() => {
+	/* setInterval(() => {
 		getUnconfirmedTrans();
-	}, 5000);
+	}, 5000); */
 });
+
+/* axios.get(
+	'http://localhost:3000/transactions',
+	{
+		headers: {Authorization: 'Basic YWRtaW0LWNlbnRz'} 
+	}
+) */ // Request format
