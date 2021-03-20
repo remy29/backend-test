60f6f695a581836e8102c57de0722f7b23e91617
remy29 <arthur.d.remy@gmail.com>
2021-03-19 17:19:41 -0700

    initial launch to heroku

--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,14 @@
-const Pool = require('pg').Pool;
 require('dotenv').config()
-const pool = new Pool({ // HERE 
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-});
+const {Pool} = require('pg');
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	ssl: isProduction,
+})
 
 const getTransactions = (request, response) => {
 	pool.query('SELECT * FROM transactions', (error, results) => {
