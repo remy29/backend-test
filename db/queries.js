0f9c6207507a259dcbc903b9e7d57be45d7bb38a
remy29 <arthur.d.remy@gmail.com>
2021-03-19 19:19:21 -0700

    test5

--- db/queries.js
+++ db/queries.js
@@ -5,7 +5,7 @@ const {Pool} = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-	ssl: true,
+	ssl: false,
 })
 
 const getTransactions = (request, response) => {
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const db = require('./db/queries');
-const sslRedirect = require('heroku-ssl-redirect').sslRedirect;
 /* const basicAuth = require('express-basic-auth') */
 const getUnconfirmedTrans = require('./helpers/api-request');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT; 
 
-app.use(sslRedirect());
 
 /* function getUnauthorizedResponse(req) {
   return req.auth
