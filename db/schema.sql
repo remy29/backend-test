c116fba36b67cd2fbec766f0806b7608e7b1c58e
remy29 <arthur.d.remy@gmail.com>
2021-03-20 21:32:07 -0700

    final commit

--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,7 @@ CREATE TABLE transactions (
 );
 
 /* below are potential additional tables, I kept the erd schema very simple to reduce in algorithmic complexity, 
-as upating the 3 tables would require a nested loop. Its not likely that one would need to query inputs/outputs independently of their transaction
-so for the sake of the excersice I decided to keep it simple. 
+as upating the 3 tables would require a nested loop.
  */
 
 /* CREATE TABLE inputs (
